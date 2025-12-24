c2b04e8eb942a0145495561c1737410526a6bc64
asuna <cherepanova.so04@gmail.com>
2025-12-24 16:34:57 +0500

    chore: widget header

---
+++ src/widgets/header/ui/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface Header {
+  heading: string;
+  icon?: ReactNode;
+}
+
+export const Header = ({ heading, icon }: Header) => {
+  return (
+    <header className="flex justify-between items-center pb-6 border-b border-b-lightgray">
+      <h1 className="text-2xl font-medium">{heading}</h1>
+      {icon}
+    </header>
+  );
+};
