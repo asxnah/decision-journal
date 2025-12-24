822b60fd19a42b8e60915f40c0e811add1d5d447
asuna <cherepanova.so04@gmail.com>
2025-12-24 16:34:30 +0500

    chore: ui radio list

---
+++ src/shared/ui/list-radio/ui/icons/radio.tsx
@@ -0,0 +1,20 @@
+interface RadioIcon {
+  checked: boolean;
+}
+
+export const RadioIcon = ({ checked }: RadioIcon) => {
+  return (
+    <svg
+      className="w-full h-full absolute"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="0.5" y="0.5" width="17" height="17" rx="8.5" fill="white" />
+      <rect x="0.5" y="0.5" width="17" height="17" rx="8.5" stroke="#6C757D" />
+      {checked && <circle cx="9" cy="9" r="5" fill="#6C757D" />}
+    </svg>
+  );
+};
---
+++ src/shared/ui/list-radio/ui/index.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState } from "react";
+import { RadioIcon } from "./icons/radio";
+
+interface List {
+  list: string[];
+  name: string;
+  onChange: (data: string) => void;
+}
+
+export const RadioList = ({ list, name, onChange }: List) => {
+  const [item, setItem] = useState("");
+
+  const handleChange = (li: string) => {
+    setItem(li);
+    onChange(li);
+  };
+
+  return (
+    <ul>
+      {list.map((li) => {
+        const meta = li.toLocaleLowerCase().replaceAll(" ", "-");
+        return (
+          <li key={li} className="flex gap-1.5">
+            <div className="w-4.5 h-4.5 relative">
+              <input
+                className="w-full h-full opacity-0 absolute z-1 cursor-pointer"
+                type="radio"
+                name={name}
+                id={meta}
+                onChange={() => handleChange(li)}
+              />
+              <RadioIcon checked={item === li} />
+            </div>
+            <label className="cursor-pointer" htmlFor={meta}>
+              {li}
+            </label>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
