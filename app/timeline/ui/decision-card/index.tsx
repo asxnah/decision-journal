1cab1667548a5a23bb40e36e127de25a82f7b2a4
asuna <cherepanova.so04@gmail.com>
2026-03-19 20:04:35 +0500

    chore(ui)

--- app/timeline/ui/decision-card/index.tsx
+++ app/timeline/ui/decision-card/index.tsx
@@ -28,12 +28,14 @@ export const DecisionCard = ({
       onClick={() => router.push(`/decision-detail/${id}`)}
     >
       <div aria-hidden={true} className="flex flex-col items-center">
-        {!firstItem && <div className="w-0.5 h-full bg-lightgray"></div>}
+        <div
+          className={`w-0.5 h-1.5 ${!firstItem ? "bg-lightgray" : ""}`}
+        ></div>
         <div className="flex-none w-3 h-3 rounded-full bg-lightgray"></div>
         {!lastItem && <div className="w-0.5 h-full bg-lightgray"></div>}
       </div>
 
-      <div className={`${!lastItem && "mb-6"} grid gap-5 flex-1`}>
+      <div className={`${!lastItem && "pb-14"} grid gap-5 flex-1`}>
         <header>
           <h2 className="font-medium text-xl">{decision}</h2>
         </header>
